5352e5d2bc43842f8878fc40bff58c5a17ef9dd5
Radu Ungurean <radu.ungurean@gmail.com>
2020-03-29 22:56:29 +0300

    change logo

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark-primary">
     <div class="container-fluid">
         <a class="navbar-brand text-white">
-            Derby.Today
+            <img src="{{URL::asset('/logo.svg')}}" />
         </a>
     </div>
 </nav>
