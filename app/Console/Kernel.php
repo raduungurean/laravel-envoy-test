557041097772467068fc8c6cec2c0cd42779c894
Radu Ungurean <radu.ungurean@gmail.com>
2020-04-04 18:43:06 +0300

    remove test command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('email:cron-email')
-            ->everyFiveMinutes();
         $schedule->command('hashes:clean')
             ->daily();
     }
