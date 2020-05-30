d8c266a816f2c4a038949d39e2b933e652bb160e
Radu Ungurean <radu.ungurean@gmail.com>
2020-05-30 21:59:33 +0300

    small fix

--- app/Repositories/DBUserRepository.php
+++ app/Repositories/DBUserRepository.php
@@ -14,13 +14,14 @@ class DBUserRepository implements UserRepository
                 INNER JOIN user_group ug on ug.group_id = g.id
                 INNER JOIN users u on u.id = ug.user_id AND u.id = :userId
                 LEFT JOIN user_group ug1 on ug1.group_id = ug.group_id
-				INNER JOIN role_user_group rug on rug.user_id = :userId AND g.id = rug.group_id
+				INNER JOIN role_user_group rug on rug.user_id = :userId1 AND g.id = rug.group_id
                 INNER JOIN roles r on r.id = rug.role_id
             WHERE g.deleted_at IS NULL
             GROUP BY ug.group_id';
 
         return DB::select( DB::raw( $sql ), array(
             'userId' => $userId,
+            'userId1' => $userId,
         ));
     }
 }
